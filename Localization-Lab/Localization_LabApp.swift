6503251ddd0aa14176956b92d1a1dfaaeef9a560
Mateus Pires <mateusmp.df@gmail.com>
2026-03-19 18:28:33 -0300

    Initial Commit

---
+++ Localization-Lab/Localization_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Localization_LabApp.swift
+//  Localization-Lab
+//
+//  Created by Mateus Martins Pires on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Localization_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
